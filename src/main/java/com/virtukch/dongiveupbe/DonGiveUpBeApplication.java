5734fd0cd616ce2540d0268677a539cf5bc89673
Virtus_Chae <ybd4731@gmail.com>
2024-10-10 14:54:49 +0900

    first commit

---
+++ src/main/java/com/virtukch/dongiveupbe/DonGiveUpBeApplication.java
@@ -0,0 +1,13 @@
+package com.virtukch.dongiveupbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonGiveUpBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonGiveUpBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/virtukch/dongiveupbe/DonGiveUpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtukch.dongiveupbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonGiveUpBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
