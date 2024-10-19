92d418f0bfcca78f013bd07a581c530894061c80
Virtus_Chae <ybd4731@gmail.com>
2024-10-20 03:09:26 +0900

    feat: Member Entity 개발

--- src/main/java/com/virtukch/dongiveupbe/member/entity/Member.java
+++ src/main/java/com/virtukch/dongiveupbe/member/entity/Member.java
@@ -4,11 +4,32 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Member {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long memberId;
-    public String memberName;
+    private Long memberId;
+    private String memberEmail;
+    private String memberSchool;
+    private String memberName;
+    private Integer memberGrade;
+    private Integer memberClass;
+    private Integer memberNumber;
+
+    @Builder
+    public Member(String memberEmail, String memberSchool, String memberName, Integer memberGrade, Integer memberClass, Integer memberNumber) {
+        this.memberEmail = memberEmail;
+        this.memberSchool = memberSchool;
+        this.memberName = memberName;
+        this.memberGrade = memberGrade;
+        this.memberClass = memberClass;
+        this.memberNumber = memberNumber;
+    }
 }
