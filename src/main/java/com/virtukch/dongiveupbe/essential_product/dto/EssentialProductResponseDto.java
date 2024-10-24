1e994418cabd44c73bc15fc66f6760c25cbdc307
hojin0729 <0918syj@gmail.com>
2024-10-25 01:16:33 +0900

    Feat: 필수 상품 CRUD 구현

--- src/main/java/com/virtukch/dongiveupbe/essential_product/dto/EssentialProductResponseDto.java
+++ src/main/java/com/virtukch/dongiveupbe/essential_product/dto/EssentialProductResponseDto.java
@@ -1,4 +1,23 @@
 package com.virtukch.dongiveupbe.essential_product.dto;
 
+import com.virtukch.dongiveupbe.essential_product.entity.EssentialProduct;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class EssentialProductResponseDto {
+    private Long essentialProductId;
+    private String essentialProductName;
+    private Long essentialProductHp;
+    private String essentialProductDescription;
+
+    public static EssentialProductResponseDto fromEntity(EssentialProduct product) {
+        return EssentialProductResponseDto.builder()
+                .essentialProductId(product.getEssentialProductId())
+                .essentialProductName(product.getEssentialProductName())
+                .essentialProductHp(product.getEssentialProductHp())
+                .essentialProductDescription(product.getEssentialProductDescription())
+                .build();
+    }
 }
