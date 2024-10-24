7c5ef2783b5280fe0162cc0c7eac2ce3324748ce
Virtus_Chae <ybd4731@gmail.com>
2024-10-24 17:55:36 +0900

    DDL 추가

--- src/main/java/com/virtukch/dongiveupbe/stock_status/repository/StockStatusRepository.java
+++ src/main/java/com/virtukch/dongiveupbe/stock_status/repository/StockStatusRepository.java
@@ -9,11 +9,4 @@ import java.util.List;
 
 public interface StockStatusRepository extends JpaRepository<StockStatus, Long> {
 
-    // 특정 라운드에서의 모든 주식 상태 조회
-    @Query("SELECT ss FROM StockStatus ss WHERE ss.round_id= :roundId")
-    List<StockStatus> findAllByRoundId(@Param("roundId") Long roundId);
-
-    // 특정 주식의 특정 라운드에서의 상태 조회
-    @Query("SELECT ss FROM StockStatus ss WHERE ss.stock_id = :stockId AND ss.round_id = :roundId")
-    StockStatus findByStockIdAndRoundId(@Param("stockId") Long stockId, @Param("roundId") Long roundId);
 }
