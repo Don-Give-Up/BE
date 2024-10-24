38dcb3f3ebf37ad7368457ebceb5eedf7ad0c7b9
hojin0729 <0918syj@gmail.com>
2024-10-24 20:55:11 +0900

    Feat: 필수상품 스텁데이터 추가

--- src/main/java/com/virtukch/dongiveupbe/essential_product/repository/EssentialProductRepository.java
+++ src/main/java/com/virtukch/dongiveupbe/essential_product/repository/EssentialProductRepository.java
@@ -1,4 +1,7 @@
 package com.virtukch.dongiveupbe.essential_product.repository;
 
-public interface EssentialProductRepository {
+import com.virtukch.dongiveupbe.essential_product.entity.EssentialProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EssentialProductRepository extends JpaRepository<EssentialProduct, Long> {
 }
