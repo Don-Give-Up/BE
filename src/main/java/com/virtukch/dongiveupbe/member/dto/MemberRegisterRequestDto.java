6193810842fed4e079ca3b5d74f8fd22ffd52a66
Virtus_Chae <ybd4731@gmail.com>
2024-10-24 21:44:27 +0900

    Member Entity 수정 완료

--- src/main/java/com/virtukch/dongiveupbe/member/dto/MemberRegisterRequestDto.java
+++ src/main/java/com/virtukch/dongiveupbe/member/dto/MemberRegisterRequestDto.java
@@ -1,5 +1,7 @@
 package com.virtukch.dongiveupbe.member.dto;
 
+import com.virtukch.dongiveupbe.member.entity.MemberRole;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,9 +13,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberRegisterRequestDto {
     private String memberEmail;
-    private String memberSchool;
+
+    private String memberPassword;
+
     private String memberName;
+
+    private String memberSchool;
+
+    private LocalDate memberDateTime;
+
+    private String memberNickname;
+
+    private MemberRole memberRole;
+
     private Integer memberGrade;
+
     private Integer memberClass;
-    private Integer memberNumber;
 }
--- src/main/java/com/virtukch/dongiveupbe/member/repository/MemberRepository.java
+++ src/main/java/com/virtukch/dongiveupbe/member/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package com.virtukch.dongiveupbe.member.repository;
 
 import com.virtukch.dongiveupbe.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Optional<Member> findByMemberEmail(String email);
 }
