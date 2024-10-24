b544e7d45ebeccb39dd72726cc6408cedef5f59e
hojin0729 <0918syj@gmail.com>
2024-10-25 03:09:58 +0900

    Feat: 게임 멤버 get만 구현

--- src/main/java/com/virtukch/dongiveupbe/game_member/service/GameMemberService.java
+++ src/main/java/com/virtukch/dongiveupbe/game_member/service/GameMemberService.java
@@ -1,4 +1,21 @@
 package com.virtukch.dongiveupbe.game_member.service;
 
+import com.virtukch.dongiveupbe.game_member.entity.GameMember;
+import com.virtukch.dongiveupbe.game_member.repository.GameMemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class GameMemberService {
+    private final GameMemberRepository gameMemberRepository;
+
+    public GameMemberService(GameMemberRepository gameMemberRepository) {
+        this.gameMemberRepository = gameMemberRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public GameMember findById(Long gameMemberId) {
+        return gameMemberRepository.findById(gameMemberId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 GameMember ID입니다: " + gameMemberId));
+    }
 }
