11a271944d225c424b8f3bd81fe4a8c2b4b30588
hojin0729 <0918syj@gmail.com>
2024-10-25 11:18:03 +0900

    Feat: 게임엔티티 빌더 패턴 추가

--- src/main/java/com/virtukch/dongiveupbe/game/entity/Game.java
+++ src/main/java/com/virtukch/dongiveupbe/game/entity/Game.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,4 +21,11 @@ public class Game {
     private String gameName;
 
     private String gamePassword;
+
+    @Builder
+    public Game(Long memberId, String gameName, String gamePassword) {
+        this.memberId = memberId;
+        this.gameName = gameName;
+        this.gamePassword = gamePassword;
+    }
 }
