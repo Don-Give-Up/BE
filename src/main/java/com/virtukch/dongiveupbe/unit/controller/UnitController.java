0b67b1ec3e302dd450171260a5994b0b3d8422a1
Virtus_Chae <ybd4731@gmail.com>
2024-10-27 11:38:33 +0900

    docs: 단원 API 명세서 추가

--- src/main/java/com/virtukch/dongiveupbe/unit/controller/UnitController.java
+++ src/main/java/com/virtukch/dongiveupbe/unit/controller/UnitController.java
@@ -3,6 +3,11 @@ package com.virtukch.dongiveupbe.unit.controller;
 import com.virtukch.dongiveupbe.unit.dto.UnitRegisterRequestDto;
 import com.virtukch.dongiveupbe.unit.dto.UnitResponseDto;
 import com.virtukch.dongiveupbe.unit.service.UnitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/units")
+@Tag(name = "단원 API", description = "문제가 속할 단원을 미리 만들고 관리하기 위한 API")
 public class UnitController {
 
     private final UnitService unitService;
@@ -25,16 +31,28 @@ public class UnitController {
     }
 
     @GetMapping
-    public ResponseEntity<List<UnitResponseDto>> getUnits() {
+    @Operation(summary = "현재 존재하는 단원 전체 조회")
+    @ApiResponse(responseCode = "200", description = "단원 전체 조회 성공",
+        content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = UnitResponseDto.class)))
+    public ResponseEntity<List<UnitResponseDto>> findAll() {
         return ResponseEntity.ok(unitService.findAll());
     }
 
     @PostMapping
+    @Operation(summary = "단원 추가")
+    @ApiResponse(responseCode = "200", description = "단원 추가 성공",
+        content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = UnitResponseDto.class)))
     public ResponseEntity<UnitResponseDto> save(@RequestBody UnitRegisterRequestDto unitRegisterRequestDto) {
         return ResponseEntity.ok(unitService.save(unitRegisterRequestDto));
     }
 
     @GetMapping("{unitName}")
+    @Operation(summary = "단원 이름으로 단원 ID & 단원 이름 찾기")
+    @ApiResponse(responseCode = "200", description = "단원 단일 조회 성공",
+        content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = UnitResponseDto.class)))
     public ResponseEntity<UnitResponseDto> findByUnitName(@PathVariable String unitName) {
         return ResponseEntity.ok(unitService.findByUnitName(unitName));
     }
