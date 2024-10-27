a9f198976864955c1b6036031816e0dbb8bcb905
hojin0729 <0918syj@gmail.com>
2024-10-27 12:06:01 +0900

    Feat: 각 엔티티 서비스 예외처리 추가

--- src/main/java/com/virtukch/dongiveupbe/essential_product_status/service/EssentialProductStatusService.java
+++ src/main/java/com/virtukch/dongiveupbe/essential_product_status/service/EssentialProductStatusService.java
@@ -1,5 +1,6 @@
 package com.virtukch.dongiveupbe.essential_product_status.service;
 
+import com.virtukch.dongiveupbe.essential_product.exception.EntityNotFoundException;
 import com.virtukch.dongiveupbe.essential_product_status.dto.EssentialProductStatusRegisterRequestDto;
 import com.virtukch.dongiveupbe.essential_product_status.dto.EssentialProductStatusResponseDto;
 import com.virtukch.dongiveupbe.essential_product_status.entity.EssentialProductStatus;
@@ -20,7 +21,7 @@ public class EssentialProductStatusService {
     @Transactional(readOnly = true)
     public EssentialProductStatus findById(Long essentialProductStatusId) {
         return essentialProductStatusRepository.findById(essentialProductStatusId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 EssentialProductStatus ID입니다: " + essentialProductStatusId));
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 EssentialProductStatus ID입니다: " + essentialProductStatusId));
     }
 
     @Transactional(readOnly = true)
@@ -40,7 +41,7 @@ public class EssentialProductStatusService {
     @Transactional
     public EssentialProductStatusResponseDto updateProductStatusAmount(Long statusId, Long amount) {
         EssentialProductStatus productStatus = essentialProductStatusRepository.findById(statusId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 상품 상태를 찾을 수 없습니다. ID: " + statusId));
+                .orElseThrow(() -> new EntityNotFoundException("해당 ID의 상품 상태를 찾을 수 없습니다. ID: " + statusId));
         productStatus = EssentialProductStatus.builder()
                 .essentialProductStatusId(productStatus.getEssentialProductStatusId())
                 .essentialProductId(productStatus.getEssentialProductId())
--- src/main/java/com/virtukch/dongiveupbe/inventory/service/InventoryService.java
+++ src/main/java/com/virtukch/dongiveupbe/inventory/service/InventoryService.java
@@ -1,5 +1,6 @@
 package com.virtukch.dongiveupbe.inventory.service;
 
+import com.virtukch.dongiveupbe.essential_product.exception.EntityNotFoundException;
 import com.virtukch.dongiveupbe.inventory.dto.InventoryRegisterRequestDto;
 import com.virtukch.dongiveupbe.inventory.dto.InventoryResponseDto;
 import com.virtukch.dongiveupbe.inventory.entity.Inventory;
@@ -25,7 +26,7 @@ public class InventoryService {
     @Transactional(readOnly = true)
     public InventoryResponseDto getInventory(Long inventoryId) {
         Inventory inventory = inventoryRepository.findById(inventoryId)
-                .orElseThrow(() -> new IllegalArgumentException("인벤토리 ID를 찾을 수 없습니다. ID: " + inventoryId));
+                .orElseThrow(() -> new EntityNotFoundException("인벤토리 ID를 찾을 수 없습니다. ID: " + inventoryId));
         return InventoryResponseDto.fromEntity(inventory);
     }
 }
