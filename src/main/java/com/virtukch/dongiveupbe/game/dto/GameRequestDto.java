8461db6a45724042efaa2b2a7cfcc6430206984f
hojin0729 <0918syj@gmail.com>
2024-10-27 15:14:23 +0900

    Feat: game cr 기능 추가

--- src/main/java/com/virtukch/dongiveupbe/game/dto/GameRequestDto.java
+++ src/main/java/com/virtukch/dongiveupbe/game/dto/GameRequestDto.java
@@ -1,4 +1,31 @@
 package com.virtukch.dongiveupbe.game.dto;
 
+import com.virtukch.dongiveupbe.game.entity.Game;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class GameRequestDto {
+    private Long memberId;
+
+    private String gameName;
+
+    private String gamePassword;
+
+    @Builder
+    public GameRequestDto(Long memberId, String gameName, String gamePassword) {
+        this.memberId = memberId;
+        this.gameName = gameName;
+        this.gamePassword = gamePassword;
+    }
+
+    public Game toEntity() {
+        return Game.builder()
+                .memberId(this.memberId)
+                .gameName(this.gameName)
+                .gamePassword(this.gamePassword)
+                .build();
+    }
 }
--- src/main/java/com/virtukch/dongiveupbe/game/dto/GameResponseDto.java
+++ src/main/java/com/virtukch/dongiveupbe/game/dto/GameResponseDto.java
@@ -1,4 +1,35 @@
 package com.virtukch.dongiveupbe.game.dto;
 
+import com.virtukch.dongiveupbe.game.entity.Game;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class GameResponseDto {
+    private Long gameId;
+
+    private Long memberId;
+
+    private String gameName;
+
+    private String gamePassword;
+
+    @Builder
+    public GameResponseDto(Long gameId, Long memberId, String gameName, String gamePassword) {
+        this.gameId = gameId;
+        this.memberId = memberId;
+        this.gameName = gameName;
+        this.gamePassword = gamePassword;
+    }
+
+    public static GameResponseDto fromEntity(Game game) {
+        return GameResponseDto.builder()
+                .gameId(game.getGameId())
+                .memberId(game.getMemberId())
+                .gameName(game.getGameName())
+                .gamePassword(game.getGamePassword())
+                .build();
+    }
 }
