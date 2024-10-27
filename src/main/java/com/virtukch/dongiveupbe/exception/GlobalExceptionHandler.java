46144618b728d2d5f7f7a3508f22a4280bc52845
hojin0729 <0918syj@gmail.com>
2024-10-27 12:05:37 +0900

    Feat: 글로벌 예외처리 생성

---
+++ src/main/java/com/virtukch/dongiveupbe/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.virtukch.dongiveupbe.exception;
+
+import com.virtukch.dongiveupbe.essential_product.exception.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
