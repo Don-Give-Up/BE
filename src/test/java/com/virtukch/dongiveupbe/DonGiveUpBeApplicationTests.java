13339f955e304acd6f681977432db2999448ebc8
Virtus_Chae <ybd4731@gmail.com>
2024-10-27 17:20:16 +0900

    test: GitAction 통과 위한 push

--- src/test/java/com/virtukch/dongiveupbe/DonGiveUpBeApplicationTests.java
+++ src/test/java/com/virtukch/dongiveupbe/DonGiveUpBeApplicationTests.java
@@ -2,8 +2,10 @@ package com.virtukch.dongiveupbe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"localCreate", "devCreate", "prod"})  // 필요한 프로파일 설정
 class DonGiveUpBeApplicationTests {
 
     @Test
