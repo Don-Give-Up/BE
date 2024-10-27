195591539fa1d17f60c006824cfd6b79ea057e6d
Virtus_Chae <ybd4731@gmail.com>
2024-10-27 15:23:24 +0900

    docs : Quiz API 명세서 Swagger 수정

--- src/main/java/com/virtukch/dongiveupbe/quiz/controller/QuizController.java
+++ src/main/java/com/virtukch/dongiveupbe/quiz/controller/QuizController.java
@@ -3,10 +3,6 @@ package com.virtukch.dongiveupbe.quiz.controller;
 import com.virtukch.dongiveupbe.quiz.dto.QuizResponseDto;
 import com.virtukch.dongiveupbe.quiz.service.QuizService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,28 +26,12 @@ public class QuizController {
 
     @GetMapping
     @Operation(summary = "모든 퀴즈 조회")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "퀴즈 목록 조회 성공",
-            content = @Content(mediaType = "application/json",
-                schema = @Schema(implementation = QuizResponseDto.class))),
-        @ApiResponse(responseCode = "404", description = "퀴즈가 존재하지 않음",
-            content = @Content)
-    })
     public ResponseEntity<List<QuizResponseDto>> findAll() {
         return quizService.findAll();
     }
 
     @GetMapping("{quizId}")
-    @Operation(summary = "특정 퀴즈 조회")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "퀴즈 조회 성공",
-            content = @Content(mediaType = "application/json",
-                schema = @Schema(implementation = QuizResponseDto.class))),
-        @ApiResponse(responseCode = "404", description = "해당 퀴즈가 존재하지 않음",
-            content = @Content),
-        @ApiResponse(responseCode = "400", description = "잘못된 요청 (예: 잘못된 quizId 형식)",
-            content = @Content)
-    })
+    @Operation(summary = "퀴즈 아이디로 퀴즈 조회")
     public ResponseEntity<QuizResponseDto> findById(@PathVariable Long quizId) {
         return quizService.findById(quizId);
     }
