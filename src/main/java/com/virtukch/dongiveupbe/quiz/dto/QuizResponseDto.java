3f8ddca0b40b89665fe6d2b191843f78eaf8bdd3
Virtus_Chae <ybd4731@gmail.com>
2024-10-28 23:15:31 +0900

    DTO 형식 변경

--- src/main/java/com/virtukch/dongiveupbe/quiz/dto/QuizResponseDto.java
+++ src/main/java/com/virtukch/dongiveupbe/quiz/dto/QuizResponseDto.java
@@ -1,5 +1,7 @@
 package com.virtukch.dongiveupbe.quiz.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.virtukch.dongiveupbe.quiz.entity.Quiz;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,22 +12,32 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonPropertyOrder({ "quizNum", "category", "quiz", "type", "answer", "desc", "level" }) // 원하는 순서 지정
 public class QuizResponseDto {
 
+    @JsonProperty(value = "quizNum")
+    private Long quizId;
+
+    @JsonProperty(value = "category")
     private String quizCategory;
 
+    @JsonProperty(value = "quiz")
     private String quizTitle;
 
+    @JsonProperty(value = "type")
     private String quizType;
 
+    @JsonProperty(value = "answer")
     private String quizAnswer;
 
+    @JsonProperty(value = "desc")
     private String quizDescription;
 
     private String quizLevel;
 
     public static QuizResponseDto fromEntity(Quiz quiz) {
         return QuizResponseDto.builder()
+            .quizId(quiz.getQuizId())
             .quizCategory(quiz.getQuizCategory())
             .quizTitle(quiz.getQuizTitle())
             .quizType(quiz.getQuizType())
