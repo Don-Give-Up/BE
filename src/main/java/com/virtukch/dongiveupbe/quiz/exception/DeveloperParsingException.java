0e6814e51ad0704454d77ebd666687e6110c8605
Virtus_Chae <ybd4731@gmail.com>
2024-10-28 23:06:54 +0900

    feat: Quiz 파싱 로직 추가
    정# feat: 새로운 기능 추가

---
+++ src/main/java/com/virtukch/dongiveupbe/quiz/exception/DeveloperParsingException.java
@@ -0,0 +1,8 @@
+package com.virtukch.dongiveupbe.quiz.exception;
+
+public class DeveloperParsingException extends RuntimeException {
+
+    public DeveloperParsingException(String message) {
+        super(message);
+    }
+}
