bb079fe0ce5ce361b28089001d4683093d01aca6
hojin0729 <0918syj@gmail.com>
2024-11-01 21:15:02 +0900

    feat: 선택 상품 dto 추가

--- src/main/java/com/virtukch/dongiveupbe/select_product/dto/SelectProductRequestDto.java
+++ src/main/java/com/virtukch/dongiveupbe/select_product/dto/SelectProductRequestDto.java
@@ -1,4 +1,27 @@
 package com.virtukch.dongiveupbe.select_product.dto;
 
+import com.virtukch.dongiveupbe.select_product.entity.SelectProduct;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class SelectProductRequestDto {
+    private String selectProductName;
+
+    private String selectProductDescription;
+
+    private Long selectProductViewAmount;
+
+    private String selectProductUrl;
+
+
+    public SelectProduct toEntity() {
+        return SelectProduct.builder()
+                .selectProductName(selectProductName)
+                .selectProductDescription(selectProductDescription)
+                .selectProductViewAmount(selectProductViewAmount)
+                .selectProductUrl(selectProductUrl)
+                .build();
+    }
 }
