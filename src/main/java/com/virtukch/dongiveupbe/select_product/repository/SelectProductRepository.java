9c4ff0689003517acf5b56f6d3a3bf0ae5324094
hojin0729 <0918syj@gmail.com>
2024-11-01 21:15:35 +0900

    feat: 선택 상품 레포지토리 추가

--- src/main/java/com/virtukch/dongiveupbe/select_product/repository/SelectProductRepository.java
+++ src/main/java/com/virtukch/dongiveupbe/select_product/repository/SelectProductRepository.java
@@ -3,5 +3,5 @@ package com.virtukch.dongiveupbe.select_product.repository;
 import com.virtukch.dongiveupbe.select_product.entity.SelectProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SelectProductRepository extends JpaRepository<SelectProduct, Integer> {
+public interface SelectProductRepository extends JpaRepository<SelectProduct, Long> {
 }
