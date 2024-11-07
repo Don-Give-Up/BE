b487965d0155229d356209d9563ed0d7c88603d4
hojin0729 <0918syj@gmail.com>
2024-11-07 13:40:35 +0900

    feat: TeacherQuiz 생성

---
+++ src/main/java/com/virtukch/dongiveupbe/domain/teacher_quiz/repository/TeacherQuizRepository.java
@@ -0,0 +1,13 @@
+package com.virtukch.dongiveupbe.domain.teacher_quiz.repository;
+
+import com.virtukch.dongiveupbe.domain.teacher_quiz.entity.TeacherQuiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TeacherQuizRepository extends JpaRepository<TeacherQuiz, Long> {
+    List<TeacherQuiz> findByMemberId(Long memberId);
+
+    Optional<TeacherQuiz> findByQuizId(Long quizId);
+}
