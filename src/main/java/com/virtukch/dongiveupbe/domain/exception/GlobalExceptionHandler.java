43117bb29cc91c21cd2b73a33f9380f64aa97992
hojin0729 <0918syj@gmail.com>
2024-11-12 15:17:15 +0900

    feat: Valid 추가

--- src/main/java/com/virtukch/dongiveupbe/domain/exception/GlobalExceptionHandler.java
+++ src/main/java/com/virtukch/dongiveupbe/domain/exception/GlobalExceptionHandler.java
@@ -4,8 +4,13 @@ import com.virtukch.dongiveupbe.domain.essential_product.exception.EntityNotFoun
 import com.virtukch.dongiveupbe.security.member.exception.MemberEmailAlreadyExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,5 +24,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleMemberEmailAlreadyExistException(MemberEmailAlreadyExistException e) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.value(), e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // 각 필드별 오류 메시지를 errors 맵에 추가
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
--- src/main/java/com/virtukch/dongiveupbe/domain/game/dto/GameRequestDto.java
+++ src/main/java/com/virtukch/dongiveupbe/domain/game/dto/GameRequestDto.java
@@ -1,6 +1,8 @@
 package com.virtukch.dongiveupbe.domain.game.dto;
 
 import com.virtukch.dongiveupbe.domain.game.entity.Game;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GameRequestDto {
     private Long memberId;
-
+    @NotBlank(message = "gameName은 필수 값입니다.")
     private String gameName;
-
+    @NotBlank(message = "password는 필수 값입니다.")
     private String gamePassword;
 
     @Builder
