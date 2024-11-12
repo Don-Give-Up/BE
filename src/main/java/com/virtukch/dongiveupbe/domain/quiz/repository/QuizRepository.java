ff9286f2d5528dbdb0e0c9c2d3f9e6f60f009909
hojin0729 <0918syj@gmail.com>
2024-11-12 22:02:27 +0900

    feat: quiz 페이지네이션 전체 조회 추가

--- src/main/java/com/virtukch/dongiveupbe/domain/quiz/repository/QuizRepository.java
+++ src/main/java/com/virtukch/dongiveupbe/domain/quiz/repository/QuizRepository.java
@@ -4,7 +4,20 @@ import com.virtukch.dongiveupbe.domain.quiz.entity.Quiz;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    Page<Quiz> findAll(Pageable pageable);
+    Page<Quiz> findByQuizTitleContainingIgnoreCase(String keyword, Pageable pageable);
+    Page<Quiz> findByQuizCategoryContainingIgnoreCase(String keyword, Pageable pageable);
+    Page<Quiz> findByQuizLevelContainingIgnoreCase(String keyword, Pageable pageable);
+
+    @Query("SELECT q FROM Quiz q WHERE " +
+            "LOWER(q.quizTitle) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(q.quizCategory) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(q.quizLevel) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Page<Quiz> findByAllFields(@Param("keyword") String keyword, Pageable pageable);
+
 }
