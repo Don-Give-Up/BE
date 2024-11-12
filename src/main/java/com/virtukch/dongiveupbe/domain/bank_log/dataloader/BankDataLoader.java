9b073bf27270b09cdf7788d3d0a18fc768880456
hojin0729 <0918syj@gmail.com>
2024-11-12 23:14:10 +0900

    feat: 데이터로더 추가

---
+++ src/main/java/com/virtukch/dongiveupbe/domain/bank_log/dataloader/BankDataLoader.java
@@ -0,0 +1,40 @@
+package com.virtukch.dongiveupbe.domain.bank_log.dataloader;
+
+import com.virtukch.dongiveupbe.domain.bank_log.entity.BankLog;
+import com.virtukch.dongiveupbe.domain.bank_log.repository.BankLogRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Profile({"localCreate", "devCreate", "prodCreate"})
+public class BankDataLoader implements CommandLineRunner {
+
+    private final BankLogRepository bankLogRepository;
+
+    public BankDataLoader(BankLogRepository bankLogRepository) {
+        this.bankLogRepository = bankLogRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBankLogs();
+    }
+
+    private void loadBankLogs() {
+        if (bankLogRepository.count() == 0) {
+            Long gameMemberId = 6L;
+            Long savingProductStatusId = 3L;  // savingProductStatusId를 3으로 고정
+
+            // 생성자를 통해 BankLog 생성
+            BankLog bankLog = new BankLog(gameMemberId, savingProductStatusId);
+
+            bankLogRepository.save(bankLog);
+            log.info("memberId 6에 대한 BankLog 스텁 데이터가 추가되었습니다. savingProductStatusId는 3으로 설정되었습니다.");
+        } else {
+            log.info("이미 BankLog 데이터가 존재합니다. 데이터 로드를 생략합니다.");
+        }
+    }
+}
