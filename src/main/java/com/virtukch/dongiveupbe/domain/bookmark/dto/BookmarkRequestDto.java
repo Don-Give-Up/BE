dbaa61826607afac306cb45bc7ec05911c930510
hojin0729 <0918syj@gmail.com>
2024-11-13 21:30:54 +0900

    Feat: Bookmark 조회, 추가, 삭제 로직 추가

--- src/main/java/com/virtukch/dongiveupbe/domain/bookmark/dto/BookmarkRequestDto.java
+++ src/main/java/com/virtukch/dongiveupbe/domain/bookmark/dto/BookmarkRequestDto.java
@@ -1,11 +1,13 @@
 package com.virtukch.dongiveupbe.domain.bookmark.dto;
 
 import com.virtukch.dongiveupbe.domain.bookmark.entity.Bookmark;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class BookmarkRequestDto {
     private Long memberId;
 
