cd46b85c752c04a9163d0419cd20ec174f47c138
Virtus_Chae <ybd4731@gmail.com>
2024-11-14 22:01:31 +0900

    feat: 게임 비밀번호 수정 및 강사님 퀴즈 추가

--- src/main/java/com/virtukch/dongiveupbe/domain/game/dataloader/GameDataLoader.java
+++ src/main/java/com/virtukch/dongiveupbe/domain/game/dataloader/GameDataLoader.java
@@ -2,11 +2,13 @@ package com.virtukch.dongiveupbe.domain.game.dataloader;
 
 import com.virtukch.dongiveupbe.domain.game.entity.Game;
 import com.virtukch.dongiveupbe.domain.game.repository.GameRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @Profile({"localCreate", "devCreate", "prodCreate"})
 public class GameDataLoader implements CommandLineRunner {
@@ -21,27 +23,26 @@ public class GameDataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Long memberId = 6L;
-        String gamePassword = "1234";
 
         // 게임 데이터 추가
         gameRepository.save(Game.builder()
                 .memberId(memberId)
                 .gameName("금융 퀴즈 대회")
-                .gamePassword(gamePassword)
+                .gamePassword("1234")
                 .build());
 
         gameRepository.save(Game.builder()
                 .memberId(memberId)
                 .gameName("금융 상식 마스터 도전!")
-                .gamePassword(gamePassword)
+                .gamePassword("2345")
                 .build());
 
         gameRepository.save(Game.builder()
                 .memberId(memberId)
                 .gameName("금리와 경제 퀴즈")
-                .gamePassword(gamePassword)
+                .gamePassword("3456")
                 .build());
 
-        System.out.println("Game data has been loaded.");
+        log.info("Game data has been loaded.");
     }
 }
