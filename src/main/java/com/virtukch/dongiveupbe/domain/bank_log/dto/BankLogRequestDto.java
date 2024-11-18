7299c51be06d2be69e655e72b7b80123c904680a
hojin0729 <0918syj@gmail.com>
2024-11-18 13:45:29 +0900

    Feat: banklog 엔티티 수정, totalprice 추가

--- src/main/java/com/virtukch/dongiveupbe/domain/bank_log/dto/BankLogRequestDto.java
+++ src/main/java/com/virtukch/dongiveupbe/domain/bank_log/dto/BankLogRequestDto.java
@@ -4,10 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import jakarta.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BankLogRequestDto {
+
+    @NotNull(message = "gameMemberId는 필수입니다.")
     private Long gameMemberId;
-    private Long savingProductStatusId;
+
+    @NotNull(message = "savingProductId는 필수입니다.")
+    private Long savingProductId;
+
+    @NotNull(message = "bankTotalPrice는 필수입니다.")
+    private Integer bankTotalPrice;
 }
+
