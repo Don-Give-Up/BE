b9df59132a2701280388d5119396ef24012b4f27
hojin0729 <0918syj@gmail.com>
2024-11-19 16:43:40 +0900

    Feat: 글로벌 예외처리 추가

--- src/main/java/com/virtukch/dongiveupbe/domain/exception/GlobalExceptionHandler.java
+++ src/main/java/com/virtukch/dongiveupbe/domain/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.virtukch.dongiveupbe.domain.exception;
 
 import com.virtukch.dongiveupbe.domain.essential_product.exception.EntityNotFoundException;
+import com.virtukch.dongiveupbe.domain.game_member.exception.DuplicateGameMemberException;
+import com.virtukch.dongiveupbe.domain.quiz_solve_record.exception.DuplicateQuizSolveException;
 import com.virtukch.dongiveupbe.security.member.exception.MemberEmailAlreadyExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +27,18 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.value(), e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
+    @ExceptionHandler(DuplicateGameMemberException.class) // 추가된 예외 처리 메서드
+    public ResponseEntity<ErrorResponse> handleDuplicateGameMemberException(DuplicateGameMemberException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DuplicateQuizSolveException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateQuizSolveException(DuplicateQuizSolveException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
